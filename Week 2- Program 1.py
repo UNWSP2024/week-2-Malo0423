5b5ce9bfbc539597f44a21647f2102dc107fa3f3
Malo0423 <mlorenson@students.unwsp.edu>
2024-09-14 19:37:40 -0500

    Add files via upload
    Maggie Lorenson
    Week 2 Project 1

---
+++ Week 2- Program 1.py
@@ -0,0 +1,7 @@
+def personal_information():
+    print('Maggie Lorenson')
+    print('26662 230th Street, Badger MN, 56714')
+    print('(218) 469-0775')
+    print('Business')
+
+personal_information()
